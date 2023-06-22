39e1d6bd6b527bd6aea035bc85ef408ce6a41e42
Nicoogf <nicolasgfalabella@gmail.com>
2023-06-22 00:39:44 -0300

    Se agregar rutas prametrisadas de practica

--- src/controllers/mainControllers.js
+++ src/controllers/mainControllers.js
@@ -2,7 +2,41 @@ const mainControllers = {
 
     inicio:(req,res)=>{
         res.send("Servidor funcionando")
+    },
+
+    parametro:(req,res)=>{
+        res.json( {id_ : req.params.id } )
+    },
+
+    comentarios:(req,res)=>{
+        res.send(`Seccion de comentarios`)
+    },
+
+    comentariosId:(req,res)=>{
+        const id = req.params.id
+        res.send(`Estas ubicado en la pagina del comentario  ${id}`)
+    },
+
+    calificacion: (req ,res )=>{
+        const id = req.params.id ;
+        res.send(`La calificacion del producto ${id} es positiva`)
+    },
+
+    practica: (req,res)=>{
+        let idProductos = req.params.idProductos
+        let idComentarios = req.params.idComentarios 
+
+        if( idComentarios == "undefined"){
+            res.send(`Estas mirando el listado de los comentarios del producto ${idProductos}`)
+        }
+        else{
+            res.send(`Estas parado en el producto ${idProductos} y solicitaste el comentario numero ${idComentarios}`)
+        }
     }
-}
+
+        
+
+  }
+
 
 export default mainControllers;
--- src/routes/mainRoutes.js
+++ src/routes/mainRoutes.js
@@ -3,7 +3,25 @@ import mainControllers from "../controllers/mainControllers.js";
 const router = express.Router() ;
 
 
+
+ /*Ruta Parametrizada  3*/
+router.get("/productos/:idProductos/comentarios/:idComentarios?" , mainControllers.practica)
+
+/*Ruta parametrizada 2 */
+router.get( "/comentarios/:id/calificacion" ,  mainControllers.calificacion  );
+
+/*Ruta parametrizada */
+router.get( "/comentarios/:id" ,  mainControllers.comentariosId  );
+
+/*Ruta para los comentarios */
+router.get( "/comentarios" ,  mainControllers.comentarios  );
+
+/*Ruta parametrizada*/ 
+router.get("/:id" , mainControllers.parametro ) ;
+
 router.get( "/" , mainControllers.inicio ) ;
-router.get
+
+
+
 
 export default router ;
